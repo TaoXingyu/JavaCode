f1d3f5ed1287e58f8da959f9ede536fb7c40e959
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 10:55:50 -0600

    Update DeadLock.java

--- DeadLock.java
+++ DeadLock.java
@@ -1,5 +1,12 @@
 package JavaCodePractice;
 
 public class DeadLock {
-
+	public void method1() { 
+		synchronized (String.class) { 
+			System.out.println("Aquired lock on String.class object"); 
+			synchronized (Integer.class) { 
+			System.out.println("Aquired lock on Integer.class object");
+			}
+		}
+   }
 }
