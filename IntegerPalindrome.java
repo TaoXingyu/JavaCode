f7c675c61fc29ddfe6c41e3b616518bf5a385618
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 10:46:47 -0600

    Update IntegerPalindrome.java

--- IntegerPalindrome.java
+++ IntegerPalindrome.java
@@ -1,5 +1,21 @@
 package JavaCodePractice;
 
-public class IntegerPalindrome {
+import java.util.ArrayList;
+import java.util.List;
 
+public class IntegerPalindrome {
+	 public boolean isPalindrome(int x) {
+	        if(x<0) return false;
+	        List list=new ArrayList();
+	        while(x/10!=0){
+	            list.add(x%10);
+	            x=x/10;
+	        }
+	        list.add(x);
+	        for(int i=0;i<list.size()/2;i++)
+	        {
+	            if(list.get(i)!=list.get(list.size()-i-1)) return false;
+	        }
+	        return true;
+	    }
 }
