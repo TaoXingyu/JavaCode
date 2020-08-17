8099bb2289c1b52b801f3cbe0f86ce4f54828f2d
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 11:52:40 -0600

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,5 +1,12 @@
 package JavaCodePractice;
 
 public class Reverse {
-
+	 public static String reverse(String str) {
+		    char[] chars = str.toCharArray();
+		    String reverse = "";
+		    for (int i = chars.length - 1; i >= 0; i--) {
+		      reverse += chars[i];
+		    }
+		    return reverse;
+		  }
 }
