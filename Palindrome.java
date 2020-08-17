02aad420c1fe32aebd08da1a69c0b3cb3fd3b918
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 09:47:45 -0600

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,13 @@
 package JavaCodePractice;
 
 public class Palindrome {
-
+public static void checkPalindrome(String str){
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                System.out.println("not Palindrome");
+                return;
+            }
+        }
+        System.out.println("Palindrome");
+    }
 }
