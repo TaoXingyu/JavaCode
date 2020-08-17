53752c102719761d13c19f732924a814c54e28d4
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 10:47:42 -0600

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -3,7 +3,18 @@ package JavaCodePractice;
 public class Armstrong {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+	    int c = 0, a, temp;
+	    int n = 153;// It is the number to check Armstrong
+	    temp = n;
+	    while (n > 0) {
+	        a = n % 10;
+	        n = n / 10;
+	        c = c + (a * a * a);
+	    }
+	    if (temp == c)
+	        System.out.println("armstrong number");
+	    else
+	        System.out.println("Not armstrong number");
 
 	}
 
